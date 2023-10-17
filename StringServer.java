601ff9c534139f84a5e0fa63788b051909ddc301
NairbNUCSD <146861770+NairbNUCSD@users.noreply.github.com>
2023-10-17 09:26:26 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,11 +1,28 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList; // import the ArrayList class
 
 class Handler implements URLHandler {
+    ArrayList<String> list = new ArrayList<String>(); // Create an ArrayList object
+    
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-
+            return String.format(toString(list));
+        } else if(url.getPath().contains("/add-message")){
+            String[] parameters = url.getQuery().split("=");
+            if(parameters[0].equals("s")){
+                list.add(parameters[1])
+                return String.format("String has been added.\n" + toString(list));
+            }
+            return "404 Not Found!";
+        } 
+    }
+    public String toString(ArrayList<String> list){
+        String str = "";
+        for(int i = 0; i< list.size(); i++){
+            str += (i + 1) + "." + list.get(i) + "\n";
         }
+        return str;
     }
 }
 
